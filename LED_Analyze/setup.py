7843db6a7cdcdc61ed260bd5088e406c3f08049d
pougivan <40307388+pougivan@users.noreply.github.com>
2018-06-15 16:17:56 -0700

    updated setup.py

--- LED_Analyze/setup.py
+++ LED_Analyze/setup.py
@@ -15,7 +15,9 @@ setup(
     install_requires=[
         "numpy",
         "matplotlib",
-        "openpyxl"],
+        "openpyxl",
+        ],
     classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"))
+        "License :: OSI Approved :: MIT License"
+        ))
