0b8b1e60dc8e9a65e46b68148a78f90398220d79
pougivan <40307388+pougivan@users.noreply.github.com>
2018-06-15 16:03:25 -0700

    updated setup.py

---
+++ LED_Analyze/setup.py
@@ -0,0 +1,18 @@
+"""
+LED_Analyze - setup
+Date: 15 June 2018
+By: Ivan Pougatchev
+Version: 1.0
+"""
+from setuptools import setup
+
+setup(
+    name="LED_Analyze",
+    description="Process colorimetry data",
+    url="https://github.com/pougivan/LED_Analyze",
+    author="pougivan",
+    packages=["LED_Analyze"]
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "openpyxl"])
