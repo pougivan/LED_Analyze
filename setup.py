b83f4319c96d017891f640b9828236d69f441153
pougivan <40307388+pougivan@users.noreply.github.com>
2018-06-15 17:06:29 -0700

    fix packages in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 LED_Analyze - setup
 Date: 15 June 2018
 By: Ivan Pougatchev
-Version: 1.0
+Version: 1.0.1
 """
 from setuptools import setup, find_packages
 
 setup(
     name="ledanalyze",
     description="Process colorimetry data",
-    version="1.0",
+    version="1.0.1",
     url="https://github.com/pougivan/LED_Analyze",
     author="pougivan",
     packages=find_packages(),
